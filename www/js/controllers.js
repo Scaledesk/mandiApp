ddcd9314a91d3d900a3f6a26bdf10343d89a0749
smriti <iamsmritid@gmail.com>
2016-07-22 15:06:48 +0530

    faq controller modified

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -144,7 +144,7 @@
         title: 'What is mandigate',
         text: 'It is a commodity marketplace for agricultural produce where farmers/bulk sellers/licensee agents can list their products and buyers (industrialists, wholesale dealers, etc.) can review and place their bulk orders through online portal or mobile app. The unique proposition of this model is direct ordering of the farm fresh items without multiple intermediaries charging their high margins. The buyers have option to order products from different locations from pan India. Also, farmers get vast market to sell their produce. In this model, buyers can expect the quality products as we individually go and verify the commodity before transshipment.We have integrated supply chain for timely delivery and reduction of wastage in transportation of goods.'
       },{
-        title: 'How can i update my informaiton?',
+        title: 'How can i update my information?',
         text: 'It is easy to update your MandiGate account and view your orders any time through My Account.'
 
       },{
@@ -153,10 +153,40 @@
       },{
         title: 'Can I order a product that is Out of Stock',
         text: 'Unfortunately, products listed as Out of Stock are not available for sale. Please use the Notify Me feature to be informed of the products availability with sellers on MandiGate'
+      }, {
+        title: 'I am a buyer, how do I know my order has been confirmed?',
+        text: 'Once your order has been logged, a unique order id (eg. ODR01202130213), is generated. The order then goes to seller for confirmation. Once seller counter confirms the receipt of the order, the order is considered confirmed. After this, we would need payment authorization for further processing.You will also be notified when the seller Transships product to you. Shipping details will be provided with the respective tracking number(s).'
       },{
-        title: '5',
-        text: 'Lorem ipsum dolor sit'
-      }];
+        title: 'Why do i see different prices for same product?',
+        text: 'A product could be listed under different prices. There could be sellers offering you the same produce but at a different price. That is the nature of the MandiGate marketplace, where different sellers compete for your order.'
+      },{
+        title:'Who can sell on MandiGate?',
+        text:'A Farmer/Aggregator selling agriculture product is welcome. In order to start selling, you need to have the following:PAN Card (Personal PAN for business type “Proprietorship” and Personal + Business PAN for business type as “Company”)Bank account and supporting KYC documents (Address Proof, and Cancelled cheque)Minimum 1000 kg of product to sale.To sell on MandiGate:1. Register yourself at MandiGate.com2. Post your product (s) under specific product categories.3. Once a buyer place order for your posting, confirm the order with quantity/Availability. Our Quality inspector will check the product quality & pack the product and mark it as ‘Ready to Dispatch’. Our logistics partner will load the product and deliver it to the Buyer.4. Once an order is successfully dispatched, MandiGate will settle your payment within 7-14 business days.  '
+      },{
+        title:'Who takes care of the delivery of my produce?',
+        text:'Our logistics partner will pick up the product from you and deliver it to the customer. All you need to do is keep it packed and ready for dispatch.'
+      },{
+        title:'How and when will I get paid?',
+        text:'The payment will be made directly to your bank account through NEFT transactions within 7-14 business days of dispatching an order. The actual payment period will vary depending on how long you have been selling at MandiGate, your customer ratings and number of orders fulfilled.'
+      },{
+        title:'When can I start selling?',
+        text:'After all the required documents have been verified and your seller profile is complete, you can start posting your products and start selling.'
+      },{
+        title:'How do I manage my orders on MandiGate?' ,
+        text:'1. Through our seller dashboard, we make it really easy for you to manage your orders. Whenever a buyer places an order, we send you a sms alert. Once an order is received, confirm the order with quantity/Availability. Our Quality inspector will check the product quality & pack the product and mark it as ‘Ready to Dispatch’. Our logistics partner will load the produce and deliver it to the Buyer.'
+
+      }, {
+        title:'How do I post my products on MandiGate?',
+        text:'We give you a step-by-step process of how to post your product on MnadiGate.com. It is important to choose the suitable category to list your produce as it will help buyer find your product faster. Based on the category you choose you will be asked to include product details such as variety, type, quantity, price, availability etc.'
+      },{
+        title:'Does MandiGate buy or sell products themselves such as Grain, Fruits or Vegetables or even Machinery?',
+        text:'No, MandiGate does not buy or sell any Product. It is an online market place and provide a supply chain Service for the products traded on the platform. MandiGate has a huge database of seller & buyer for Products and Services and we can help you connect with them with ease for your business needs.'
+      },{
+        title:'How soon will my enquiry be tended to?' ,
+        text:'Your enquiry would be tended to within a maximum of 48 Hours.'
+
+      }
+      ];
 
       /*
        * if given group is the selected group, deselect it
